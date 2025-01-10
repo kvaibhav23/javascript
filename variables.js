3bb8bf97c4fcb6bbf035e480c23db709a5ae10bd
Vaibhav kumar <vaibhavk6392@gmail.com>
2025-01-10 18:18:03 +0530

    Done

---
+++ variables.js
@@ -0,0 +1,7 @@
+const accountId=14453
+let accountEmail = "rahul123@gmail.com"
+var accountPassword = "123456"
+accountCity="JAipur"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
